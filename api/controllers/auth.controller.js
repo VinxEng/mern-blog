1d9b789078be715edb1eb7d7d5404b91e06ca0e9
VinxEng <vinx.engineer@gmail.com>
2024-06-24 15:58:36 +0700

    SignIn API route + Signin Page Functionality.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { errorHandle } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
 	// console.log(req.body);
@@ -28,6 +29,41 @@ export const signup = async (req, res, next) => {
 	try {
 		await newUser.save();
 		res.json({ msg: "Signup Success!" });
+	} catch (error) {
+		next(error);
+	}
+};
+
+export const signin = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (!email || !password || email === "" || password === "") {
+		next(errorHandle(400, "Hãy điền đầy đủ thông tin."));
+	}
+
+	try {
+		const validUser = await User.findOne({ email });
+		if (!validUser) {
+			return next(errorHandle(404, "Email không hợp lệ!"));
+		}
+		const validPass = bcrypt.compareSync(password, validUser.password);
+		if (!validPass) {
+			return next(errorHandle(400, "Password không hợp lệ."));
+		}
+
+		const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+		//* Dòng này sử dụng tính năng phá hủy đối tượng để tạo một phần còn lại đối tượng mới.
+		//* Loại trừ thuộc tính mật khẩu khỏi đối tượng validUser._doc.
+		//* Điều này đảm bảo mật khẩu không được gửi lại cho khách hàng trong phản hồi.
+		const { password: pass, ...rest } = validUser._doc;
+		res
+			.status(200)
+			.cookie("access_token", token, {
+				//* Cờ httpOnly đảm bảo JavaScript không thể truy cập cookie ở phía máy khách vì lý do bảo mật.
+				httpOnly: true,
+			})
+			.json(rest);
 	} catch (error) {
 		next(error);
 	}
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,116 @@
-import React from "react";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignIn = () => {
-	return <div>SignIN</div>;
+	const [formData, setFormData] = useState({});
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const navigate = useNavigate();
+	/**
+	 *
+	 * @param {React.ChangeEvent<HTMLInputElement>} e
+	 */
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+		// console.log(formData);
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (!formData.email || !formData.password) {
+			return setErrorMessage("Hãy điền đầy đủ thông tin.");
+		}
+		try {
+			setLoading(true);
+			setErrorMessage(null);
+			const res = await fetch("/api/auth/signin", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(formData),
+			});
+			const data = await res.json();
+			if (data.success === false) {
+				return setErrorMessage(data.message);
+			}
+			setLoading(false);
+			if (res.ok) {
+				navigate("/");
+			}
+		} catch (error) {
+			setErrorMessage(error.message);
+			setLoading(false);
+		}
+	};
+
+	return (
+		<div className="min-h-screen mt-20">
+			{/* Left */}
+			<div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+				<div className="flex-1">
+					<Link
+						to="/"
+						className="font-logo font-bold self-center text-4xl dark:text-white"
+					>
+						Vinx<span className="text-brand-700">&nbsp;Blog's</span>
+					</Link>
+					<p className="text-sm mt-5">
+						This is a demo project. You can sign in with your email and password
+						or with Google.
+					</p>
+				</div>
+				{/* Right */}
+				<div className="flex-1">
+					<form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+						<div>
+							<Label value="Your Email"></Label>
+							<TextInput
+								type="email"
+								placeholder="name@company.com"
+								id="email"
+								onChange={handleChange}
+							/>
+						</div>
+						<div>
+							<Label value="Your Password"></Label>
+							<TextInput
+								type="password"
+								placeholder="********"
+								id="password"
+								onChange={handleChange}
+							/>
+						</div>
+
+						<Button
+							gradientDuoTone="purpleToPink"
+							type="submit"
+							disabled={loading}
+						>
+							{loading ? (
+								<>
+									<Spinner size="sm" />
+									<span className="pl-3">Loading...</span>
+								</>
+							) : (
+								"Sign In"
+							)}
+						</Button>
+					</form>
+					<div className="flex gap-2 text-sm mt-5">
+						<span>Don't have an account?&nbsp;</span>
+						<Link to="/sign-up" className="text-blue-500">
+							Sign Up
+						</Link>
+					</div>
+					{errorMessage && (
+						<Alert className="mt-5" color="failure">
+							{errorMessage}
+						</Alert>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SignIn;
