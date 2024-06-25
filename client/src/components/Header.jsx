68df2c1fd54d18cb1e97d4c9fa6b10581e5892ca
VinxEng <vinx.engineer@gmail.com>
2024-06-25 13:39:25 +0700

    Update Header component and complete Dark mode.

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,33 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
 import { pageLinks } from "../constants/contents";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTheme } from "../redux/theme/themeSlice.js";
 
 const Header = () => {
 	const path = useLocation().pathname;
+	const dispatch = useDispatch();
+	const { currentUser } = useSelector((state) => state.user);
+	const { theme } = useSelector((state) => state.theme);
+
+	const handleSignout = async () => {
+		try {
+			const res = await fetch("/api/user/signout", {
+				method: "POST",
+			});
+			const data = await res.json();
+			if (!res.ok) {
+				console.log(data.message);
+			} else {
+				dispatch(signoutSuccess());
+			}
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
 	return (
 		<div>
 			<Navbar className="border-b-2">
@@ -28,14 +50,44 @@ const Header = () => {
 				</Button>
 
 				<div className="flex gap-2 md:order-2">
-					<Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
-						<FaMoon />
+					<Button
+						className="w-12 h-10 hidden sm:inline"
+						color="gray"
+						pill
+						onClick={() => dispatch(toggleTheme())}
+					>
+						{theme === "light" ? <FaSun /> : <FaMoon />}
 					</Button>
-					<Link to="/sign-in">
-						<Button gradientDuoTone="purpleToBlue" outline>
-							Sign In
-						</Button>
-					</Link>
+					{/* User Login */}
+					{currentUser ? (
+						<Dropdown
+							arrowIcon={false}
+							inline
+							label={
+								<Avatar alt="user" img={currentUser.profilePicture} rounded />
+							}
+						>
+							<Dropdown.Header>
+								<span className="block text-sm">@{currentUser.username}</span>
+								<span className="block text-sm font-medium truncate">
+									{currentUser.email}
+								</span>
+							</Dropdown.Header>
+
+							<Link to={"/dashboard?tab=profile"}>
+								<Dropdown.Item>Profile</Dropdown.Item>
+							</Link>
+							<Dropdown.Divider />
+							<Dropdown.Item onClick={handleSignout}>Sign Out</Dropdown.Item>
+						</Dropdown>
+					) : (
+						<Link to="/sign-in">
+							<Button gradientDuoTone="purpleToBlue" outline>
+								Sign In
+							</Button>
+						</Link>
+					)}
+
 					<Navbar.Toggle />
 				</div>
 
